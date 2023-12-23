848312548d0d689b735ab88ee71427d7f3d273ba
usamafayaz <usama.fayyaz157@gmail.com>
2023-12-23 12:10:09 +0500

    Learning ML

--- Day1.py
+++ Day1.py
@@ -6,18 +6,18 @@ import math
 
 # 2. Program the counts the iteration of 10 '6' or total '10' point of dice.
 
-# totalReps=0
-# counter=0
-# while True:
-#     totalReps=totalReps+1
-#     dice1 = rd.randint(1, 6)
-#     dice2 = rd.randint(1, 6)
-#     if dice1==6 or dice1+dice2>=10 or dice2==6:
-#         counter+=1
-#     if counter ==10:
-#         break
-#
-# print("Total Reps are :", totalReps)
+totalReps=0
+counter=0
+while True:
+    totalReps=totalReps+1
+    dice1 = rd.randint(1, 6)
+    dice2 = rd.randint(1, 6)
+    if dice1==6 or dice1+dice2>=10 or dice2==6:
+        counter+=1
+    if counter ==10:
+        break
+
+print("Total Reps are :", totalReps)
 
 # 3. Dictionary in Python.
 
@@ -72,22 +72,23 @@ import math
 # DisplayData("Dictionary after Updating.")
 
 # 4. Program to make a list of students which are dropped by
-#    comparing there CGPA.
-
-students ={
-    'arid_1':['Usama',0.75,8],
-    'arid_2':['Zia',3.87,5],
-    'arid_3':['Abdullah',1.75,6],
-    'arid_4':['Anees',3.0,6],
-    'arid_5':['Adeel',2.2,7],
-}
-dropStudents={}
-dropLst=[0.75,1.25,1.75,2.25]
-for k,v in students.items():
-    if v[1]<dropLst[math.ceil((v[2]/2)-1)]:
-
-        dropStudents[k]=v
-
-print("List of Dropped Students:")
-for k,v in dropStudents.items():
-    print("We are sorry",v[0],"! Minimum requirement for you was ",dropLst[math.ceil((v[2]/2)-1)])
+# #    comparing there CGPA.
+#
+# students ={
+#     'arid_1':['Usama',0.75,8],
+#     'arid_2':['Zia',3.87,5],
+#     'arid_3':['Abdullah',1.75,6],
+#     'arid_4':['Anees',3.0,6],
+#     'arid_5':['Adeel',2.2,7],
+# }
+# dropStudents={}
+# dropLst=[0.75,1.25,1.75,2.25]
+# for k,v in students.items():
+#     if v[1]<dropLst[math.ceil((v[2]/2)-1)]:
+#
+#         dropStudents[k]=v
+#
+# print("List of Dropped Students:")
+# for k,v in dropStudents.items():
+#
+#     print("We are sorry",v[0],"! Minimum requirement for you was ",dropLst[math.ceil((v[2]/2)-1)])
